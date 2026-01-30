1872d629fce9288accfb89ed1d35ab39665f925f
Maxwell Tran <mhttran@ucdavis.edu>
2026-01-30 04:55:14 +0000

    Added documentation of XMLReader.cpp

--- src/XMLReader.cpp
+++ src/XMLReader.cpp
@@ -6,22 +6,23 @@ struct CXMLReader::SImplementation{
     XML_Parser parser;
     std::queue<SXMLEntity> entityQueue;
     std::shared_ptr<CDataSource> dataSource;
-    bool endParsing;
 
     static void ExpatStartElement(void *data, const XML_Char *name, const XML_Char **attrs) {
-        SImplementation *userData = (SImplementation *)data;
-        SXMLEntity entity;
+        SImplementation *userData = (SImplementation *)data; // allows this static function to access the member data of the current object
+        SXMLEntity entity; // declare entity to push
 
+        // fill out entity information
         entity.DType = SXMLEntity::EType::StartElement;
         entity.DNameData = name;
 
+        // add attributes to DAttributes vector of StartElement entity
         int i = 0;
         while (attrs[i] && attrs[i + 1]) {
             entity.DAttributes.push_back({attrs[i], attrs[i + 1]});
             i += 2;
         }
 
-        userData->entityQueue.push(entity);
+        userData->entityQueue.push(entity); // push entity to queue
     }
     
     static void ExpatEndElement(void *data, const XML_Char *name) {
@@ -37,6 +38,7 @@ struct CXMLReader::SImplementation{
     static void ExpatCharacterData(void *data, const XML_Char *s, int len) {
         SImplementation *userData = (SImplementation *)data;
 
+        // deals with CharData longer than buffer size of 512 characters
         if (userData->entityQueue.back().DType == SXMLEntity::EType::CharData) {
             userData->entityQueue.back().DNameData.append(s, len);
             return;
@@ -50,44 +52,44 @@ struct CXMLReader::SImplementation{
     }
 
     SImplementation(std::shared_ptr<CDataSource> src) {
+        // initialize member data
         parser = XML_ParserCreate(nullptr);
         XML_SetElementHandler(parser, ExpatStartElement, ExpatEndElement);
         XML_SetCharacterDataHandler(parser, ExpatCharacterData);
         XML_SetUserData(parser, this);
         dataSource = src;
-        endParsing = false;
     }
 
     ~SImplementation() {
-        XML_ParserFree(parser);
+        XML_ParserFree(parser); // delete/free XML parser when done
     }
 
     bool End() const {
-        return entityQueue.empty();
+        return entityQueue.empty(); // no more XML text to parse + no more XML entities to read = donea
     }
 
     bool ReadEntity(SXMLEntity &entity, bool skipcdata) {
         std::vector<char> buffer;
-        while (dataSource->Read(buffer, 512)) {
+        while (dataSource->Read(buffer, 512)) { // while XML can be read from the data source, parse it
             if (XML_Parse(parser, buffer.data(), buffer.size(), 0) == XML_STATUS_ERROR) {
-                break;
+                break; // if parser encounters error, just move on
             }
         }
-        XML_Parse(parser, nullptr, 0, 1);
+        XML_Parse(parser, nullptr, 0, 1); // final call to XML parser once all XML has been read
 
-        while (!entityQueue.empty()) {
+        while (!entityQueue.empty()) { // while there are entities in the queue, read them
             SXMLEntity entityToRead = entityQueue.front();
             entityQueue.pop();
 
             if (entityToRead.DType == SXMLEntity::EType::CharData && skipcdata) {
-                continue;
+                continue; // skip CharData entities if skipcdata is true
             }
 
             entity = entityToRead;
             return true;
         }
 
-        return false;
+        return false; // no more XML text to parse + no more XML entities to read = done
     }
 };
 
