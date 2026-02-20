62d07c435768d24f9c211ea4455745d6f010da91
Maxwell Tran <mhttran@ucdavis.edu>
2026-02-20 04:55:25 +0000

    Change XMLBusSystem tests, add XMLBusSystem implementation

--- src/XMLBusSystem.cpp
+++ src/XMLBusSystem.cpp
@@ -1,14 +1,30 @@
 #include "XMLBusSystem.h"
 #include <vector>
 #include <unordered_map>
+#include <map>
+#include <iostream>
 
 struct CXMLBusSystem::SImplementation{
     const std::string DBusSystemTag = "bussystem";
+
     const std::string DStopsTag = "stops";
     const std::string DStopTag = "stop";
     const std::string DStopIDAttr = "id";
     const std::string DStopNodeAttr = "node";
     const std::string DStopDescAttr = "description";
+
+    const std::string DRoutesTag = "routes";
+    const std::string DRouteTag = "route";
+    const std::string DRouteStopTag = "routestop";
+    const std::string DRouteNameAttr = "name";
+    const std::string DRouteStopStopAttr = "stop";
+
+    const std::string DPathsTag = "paths";
+    const std::string DPathTag = "path";
+    const std::string DPathSourceAttr = "source";
+    const std::string DPathDestAttr = "destination";
+    const std::string DPathNodeTag = "node";
+    const std::string DPathNodeIDAttr = "id";
 
     
 
@@ -24,27 +40,28 @@ struct CXMLBusSystem::SImplementation{
         }
         ~SStop(){};
         TStopID ID() const noexcept override{
-            
+            return DID;
         }
 
         CStreetMap::TNodeID NodeID() const noexcept override{
-            
+            return DNodeID;
         }
 
         std::string Description() const noexcept override{
-            
+            return DDescription;
         }
 
         std::string Description(const std::string &description) noexcept override{
-            
+            DDescription = description;
+            return DDescription;
         }
     };
 
     struct SRoute : public CBusSystem::SRoute {
         std::string DName;
-        std::vector<SStop> DStops;
+        std::vector<std::shared_ptr<SStop>> DStops;
 
-        SRoute(std::string name, std::vector<SStop> stops) {
+        SRoute(std::string name, std::vector<std::shared_ptr<SStop>> stops) {
             DName = name;
             DStops = stops;
         }
@@ -52,41 +69,49 @@ struct CXMLBusSystem::SImplementation{
         ~SRoute(){};
 
         std::string Name() const noexcept override {
-
+            return DName;
         }
 
         std::size_t StopCount() const noexcept override {
-
+            return DStops.size();
         }
 
         TStopID GetStopID(std::size_t index) const noexcept override {
+            if (index >= StopCount()) {
+                return CBusSystem::InvalidStopID;
+            }
 
+            return DStops[index]->ID();
         }
     };
 
     struct SPath : public CBusSystem::SPath {
-        std::vector<CStreetMap::SNode> DNodes;
+        std::vector<CStreetMap::TNodeID> DNodes;
 
-        SPath(std::vector<CStreetMap::SNode> nodes) {
+        SPath(std::vector<CStreetMap::TNodeID> nodes) {
             DNodes = nodes;
         }
 
         ~SPath(){};
 
         CStreetMap::TNodeID StartNodeID() const noexcept override {
-
+            return DNodes[0];
         }
 
         CStreetMap::TNodeID EndNodeID() const noexcept override {
-
+            return DNodes[NodeCount() - 1];
         }
 
         std::size_t NodeCount() const noexcept override {
-
+            return DNodes.size();
         }
 
         CStreetMap::TNodeID GetNodeID(std::size_t index) const noexcept override {
-            
+            if (index >= NodeCount()) {
+                return CStreetMap::InvalidNodeID;
+            }
+
+            return DNodes[index];
         }
     };
 
@@ -114,8 +139,15 @@ struct CXMLBusSystem::SImplementation{
     std::unordered_map<TStopID,std::shared_ptr<SStop> > DStopsByID;
 
     void ParseStop(std::shared_ptr< CXMLReader > systemsource, const SXMLEntity &stop){
-        TStopID StopID = std::stoull(stop.AttributeValue(DStopIDAttr));
-        CStreetMap::TNodeID NodeID = std::stoull(stop.AttributeValue(DStopNodeAttr));
+        std::string stopIDStr = stop.AttributeValue(DStopIDAttr);
+        std::string nodeIDStr = stop.AttributeValue(DStopNodeAttr);
+
+        if (stopIDStr.empty() || nodeIDStr.empty()) {
+            return;
+        }
+        
+        TStopID StopID = std::stoull(stopIDStr);
+        CStreetMap::TNodeID NodeID = std::stoull(nodeIDStr);
         auto NewStop = std::make_shared<SStop>(StopID, NodeID, stop.AttributeValue(DStopDescAttr));
         DStopsByIndex.push_back(NewStop);
         DStopsByID[StopID] = NewStop;
@@ -129,38 +161,116 @@ struct CXMLBusSystem::SImplementation{
             if(!systemsource->ReadEntity(TempEntity, true)){
                 return;
             }
-            if((TempEntity.DType == SXMLEntity::EType::StartElement) &&(TempEntity.DNameData == DStopsTag)){
+            if((TempEntity.DType == SXMLEntity::EType::StartElement) &&(TempEntity.DNameData == DStopTag)){
                 ParseStop(systemsource,TempEntity);
             }
 
         }while((TempEntity.DType != SXMLEntity::EType::EndElement)||(TempEntity.DNameData != DStopsTag));
     }
 
-    void ParseRoute(std::shared_ptr< CXMLReader > systemsource){
+    std::vector<std::shared_ptr<SRoute>> DRoutesByIndex;
+    std::unordered_map<std::string, std::shared_ptr<SRoute>> DRoutesByName;
 
+    std::vector<std::shared_ptr<SStop>> GetRouteStops(std::shared_ptr<CXMLReader> systemsource) {
+        std::vector<std::shared_ptr<SStop>> RouteStops;
+        SXMLEntity TempEntity;
+        
+        while (systemsource->ReadEntity(TempEntity, true) && TempEntity.DNameData == DRouteStopTag) {
+            std::string stopIDStr = TempEntity.AttributeValue(DRouteStopStopAttr);
+            if (!stopIDStr.empty()) {
+                TStopID RouteStopID = std::stoull(stopIDStr);
+                RouteStops.push_back(StopByID(RouteStopID));
+            }
+        }
+
+        return RouteStops;
+    }
+
+    void ParseRoute(std::shared_ptr< CXMLReader > systemsource, const SXMLEntity &route){
+        std::string RouteName = route.AttributeValue(DRouteNameAttr);
+        auto NewRoute = std::make_shared<SRoute>(RouteName, GetRouteStops(systemsource));
+        DRoutesByIndex.push_back(NewRoute);
+        DRoutesByName[RouteName] = NewRoute;
     }
 
     void ParseRoutes(std::shared_ptr< CXMLReader > systemsource){
+        SXMLEntity TempEntity;
 
+        do{
+            if(!systemsource->ReadEntity(TempEntity, true)){
+                return;
+            }
+            if((TempEntity.DType == SXMLEntity::EType::StartElement) &&(TempEntity.DNameData == DRouteTag)){
+                ParseRoute(systemsource,TempEntity);
+            }
+
+        }while((TempEntity.DType != SXMLEntity::EType::EndElement)||(TempEntity.DNameData != DRoutesTag));
     }
 
-    
-
     void ParseBusSystem(std::shared_ptr< CXMLReader > systemsource){
-        SXMLEntity TempEntity;
         if(!FindStartTag(systemsource,DBusSystemTag)){
             return;
         }
+
         if(!FindStartTag(systemsource,DStopsTag)){
             return;
         }
         ParseStops(systemsource);
 
+        if (!FindStartTag(systemsource, DRoutesTag)) {
+            return;
+        }
+        ParseRoutes(systemsource);
+    }
+
+    
+    std::map<std::pair<TStopID, TStopID>, std::shared_ptr<SPath>> DPathsbyStopIDs;
+
+    std::vector<CStreetMap::TNodeID> GetPathNodes(std::shared_ptr<CXMLReader> systemsource) {
+        std::vector<CStreetMap::TNodeID> PathNodes;
+        SXMLEntity TempEntity;
+        
+        while (systemsource->ReadEntity(TempEntity, true) && TempEntity.DNameData == DPathNodeTag) {
+            std::string nodeIDStr = TempEntity.AttributeValue(DPathNodeIDAttr);
+
+            if (!nodeIDStr.empty()) {
+                CStreetMap::TNodeID PathNodeID = std::stoull(TempEntity.AttributeValue(DPathNodeIDAttr));
+                PathNodes.push_back(PathNodeID);
+            }
+        }
+
+        return PathNodes;
+    }
+
+    void ParsePath(std::shared_ptr<CXMLReader> pathsource, const SXMLEntity &path) {
+        auto NewPath = std::make_shared<SPath>(GetPathNodes(pathsource));
+        DPathsbyStopIDs[{NewPath->StartNodeID(), NewPath->EndNodeID()}] = NewPath;
+    }
+
+    void ParsePaths(std::shared_ptr<CXMLReader> pathsource) {
+        SXMLEntity TempEntity;
+
+        do {
+            if (!pathsource->ReadEntity(TempEntity, true)) {
+                return;
+            }
+            if (TempEntity.DType == SXMLEntity::EType::StartElement && TempEntity.DNameData == DPathTag) {
+                ParsePath(pathsource, TempEntity);
+            }
+        } while (TempEntity.DType != SXMLEntity::EType::EndElement || TempEntity.DNameData != DPathsTag);
+    }
+
+    void ParsePathSystem(std::shared_ptr<CXMLReader> pathsource) {
+        if (!FindStartTag(pathsource, DPathsTag)) {
+            return;
+        }
+
+        ParsePaths(pathsource);
     }
 
     SImplementation(std::shared_ptr< CXMLReader > systemsource, std::shared_ptr< CXMLReader > pathsource){
         ParseBusSystem(systemsource);
-        
+        ParsePathSystem(pathsource);
     }
 
     std::size_t StopCount() const noexcept{
@@ -168,27 +278,57 @@ struct CXMLBusSystem::SImplementation{
     }
 
     std::size_t RouteCount() const noexcept{
-
+        return DRoutesByIndex.size();
     }
     
     std::shared_ptr<SStop> StopByIndex(std::size_t index) const noexcept{
+        if (index >= StopCount()) {
+            return nullptr;
+        }
         
+        return DStopsByIndex[index];
     }
     
     std::shared_ptr<SStop> StopByID(TStopID id) const noexcept{
-
+        if (DStopsByID.count(id) == 0) {
+            return nullptr;
+        }
+        
+        return DStopsByID.at(id);
     }
     
     std::shared_ptr<SRoute> RouteByIndex(std::size_t index) const noexcept{
+        if (index >= RouteCount()) {
+            return nullptr;
+        }
 
+        return DRoutesByIndex[index];
     }
     
     std::shared_ptr<SRoute> RouteByName(const std::string &name) const noexcept{
+        if (DRoutesByName.count(name) == 0) {
+            return nullptr;
+        }
 
+        return DRoutesByName.at(name);
     }
     
     std::shared_ptr<SPath> PathByStopIDs(TStopID start, TStopID end) const noexcept{
+        auto startStop = StopByID(start);
+        auto endStop = StopByID(end);
 
+        if (!startStop || !endStop) {
+            return nullptr;
+        }
+
+        CStreetMap::TNodeID startStopNode = startStop->NodeID();
+        CStreetMap::TNodeID endStopNode = endStop->NodeID();
+        
+        if (DPathsbyStopIDs.count({startStopNode, endStopNode}) == 0) {
+            return nullptr;
+        }
+        
+        return DPathsbyStopIDs.at({startStopNode, endStopNode});
     }
     
 };
@@ -206,7 +346,7 @@ std::size_t CXMLBusSystem::StopCount() const noexcept{
 }
     
 std::size_t CXMLBusSystem::RouteCount() const noexcept{
-
+    return DImplementation->RouteCount();
 }
 
 std::shared_ptr<CBusSystem::SStop> CXMLBusSystem::StopByIndex(std::size_t index) const noexcept{
@@ -218,13 +358,13 @@ std::shared_ptr<CBusSystem::SStop> CXMLBusSystem::StopByID(TStopID id) const noe
 }
 
 std::shared_ptr<CBusSystem::SRoute> CXMLBusSystem::RouteByIndex(std::size_t index) const noexcept{
-
+    return DImplementation->RouteByIndex(index);
 }
 
 std::shared_ptr<CBusSystem::SRoute> CXMLBusSystem::RouteByName(const std::string &name) const noexcept{
-
+    return DImplementation->RouteByName(name);
 }
 
 std::shared_ptr<CBusSystem::SPath> CXMLBusSystem::PathByStopIDs(TStopID start, TStopID end) const noexcept{
-
+    return DImplementation->PathByStopIDs(start, end);
 }
--- testsrc/XMLBusSystemTest.cpp
+++ testsrc/XMLBusSystemTest.cpp
@@ -3,17 +3,22 @@
 #include "StringDataSource.h"
 
 TEST(XMLBusSystemTest, SimpleTest) {
-    // Beginning of professor's code from 2/6/2026 discussion (Test stops)
     auto BusRouteSource = std::make_shared<CStringDataSource>("<bussystem>\n"
                                                                 "<stops>\n"
                                                                     "<stop id=\"1\" node=\"321\" description=\"First\"/>\n"
                                                                     "<stop id=\"2\" node=\"311\" description=\"second\"/>\n"
                                                                 "</stops>\n"
+                                                                "<routes>\n"
+                                                                    "<route name=\"M\">\n"
+                                                                        "<routestop stop=\"1\" />\n"
+                                                                        "<routestop stop=\"2\" />\n"
+                                                                    "</route>\n"
+                                                                "</routes>\n"
                                                             "</bussystem>");
     auto BusRouteReader = std::make_shared<CXMLReader>(BusRouteSource);
 
     auto BusPathSource = std::make_shared<CStringDataSource>("<paths>\n"
-                                                                "<path source =\"321\" destination=\"311\"/>\n"
+                                                                "<path source =\"321\" destination=\"311\">\n"
                                                                     "<node id=\"321\"/>\n"
                                                                     "<node id=\"315\"/>\n"
                                                                     "<node id=\"311\"/>\n"
@@ -23,8 +28,8 @@ TEST(XMLBusSystemTest, SimpleTest) {
 
     CXMLBusSystem BusSystem(BusRouteReader, BusPathReader);
 
+    // Test stops
     ASSERT_EQ(BusSystem.StopCount(), 2);
-    EXPECT_EQ(BusSystem.RouteCount(), 0);
     EXPECT_NE(BusSystem.StopByIndex(0), nullptr);
     EXPECT_NE(BusSystem.StopByIndex(1), nullptr);
     
@@ -33,17 +38,28 @@ TEST(XMLBusSystemTest, SimpleTest) {
     EXPECT_EQ(StopObj->ID(), 1);
     EXPECT_EQ(StopObj->NodeID(), 321);
     EXPECT_EQ(StopObj->Description(), "First");
+    EXPECT_EQ(StopObj->Description("first"), "first");
 
     StopObj = BusSystem.StopByID(2);
     ASSERT_NE(StopObj, nullptr);
     EXPECT_EQ(StopObj->ID(), 2);
     EXPECT_EQ(StopObj->NodeID(), 311);
     EXPECT_EQ(StopObj->Description(), "second");
-    // End of professor's code from 2/6/2026 discussion
+    EXPECT_EQ(StopObj->Description("Second"), "Second");
 
-    // Test routes (no routes here, see RouteTest)
-    EXPECT_EQ(BusSystem.RouteByIndex(0), nullptr);
+    // Test routes
+    ASSERT_EQ(BusSystem.RouteCount(), 1);
+    EXPECT_NE(BusSystem.RouteByIndex(0), nullptr);
+    EXPECT_EQ(BusSystem.RouteByIndex(1), nullptr);
     EXPECT_EQ(BusSystem.RouteByName("placeholder"), nullptr);
+
+    auto RouteObj = BusSystem.RouteByName("M");
+    ASSERT_NE(RouteObj, nullptr);
+    EXPECT_EQ(RouteObj->Name(), "M");
+    EXPECT_EQ(RouteObj->StopCount(), 2);
+    EXPECT_EQ(RouteObj->GetStopID(0), 1);
+    EXPECT_EQ(RouteObj->GetStopID(1), 2);
+    EXPECT_EQ(RouteObj->GetStopID(2), CBusSystem::InvalidStopID);
 
     // Test paths
     auto PathObj = BusSystem.PathByStopIDs(1, 2);
@@ -57,46 +73,8 @@ TEST(XMLBusSystemTest, SimpleTest) {
     EXPECT_EQ(PathObj->GetNodeID(3), CStreetMap::InvalidNodeID);
 }
 
-TEST(XMLBusSystemTest, RouteTest) {
-    auto BusRouteSource = std::make_shared<CStringDataSource>("<bussystem>\n"
-                                                                "<routes>\n"
-                                                                    "<route name=\"M\"\n>"
-                                                                        "<routestop stop=\"22272\" />\n"
-                                                                        "<routestop stop=\"22014\" />\n"
-                                                                        "<routestop stop=\"22002\" />\n"
-                                                                    "</route>\n"
-                                                                "</routes>\n"
-                                                            "</bussystem>");
-    auto BusRouteReader = std::make_shared<CXMLReader>(BusRouteSource);
-
-    // path reader is just a placeholder for the CXMLBusSystem
-    auto BusPathSource = std::make_shared<CStringDataSource>("<paths>\n"
-                                                                "<path source=\"321\" destination=\"311\"/>\n"
-                                                                    "<node id=\"321\"/>\n"
-                                                                    "<node id=\"315\"/>\n"
-                                                                    "<node id=\"311\"/>\n"
-                                                                "</path>\n"
-                                                            "</paths>");
-    auto BusPathReader = std::make_shared<CXMLReader>(BusPathSource);
-
-    CXMLBusSystem BusSystem(BusRouteReader, BusPathReader);
-
-    ASSERT_EQ(BusSystem.RouteCount(), 1);
-    EXPECT_NE(BusSystem.RouteByIndex(0), nullptr);
-    EXPECT_EQ(BusSystem.RouteByIndex(1), nullptr);
-
-    auto RouteObj = BusSystem.RouteByName("M");
-    ASSERT_NE(RouteObj, nullptr);
-    EXPECT_EQ(RouteObj->Name(), "M");
-    EXPECT_EQ(RouteObj->StopCount(), 3);
-    EXPECT_EQ(RouteObj->GetStopID(0), 22272);
-    EXPECT_EQ(RouteObj->GetStopID(1), 22014);
-    EXPECT_EQ(RouteObj->GetStopID(2), 22002);
-    EXPECT_EQ(RouteObj->GetStopID(3), CBusSystem::InvalidStopID);
-}
-
 TEST(XMLBusSystemTest, InvalidXMLTest) {
-    // Test what happens if error is at the beginning (shouldn't read anything)
+    // Test what happens if error is at the beginning
     auto BusRouteSource = std::make_shared<CStringDataSource>("<bussystem\n"
                                                                 "<stops>\n"
                                                                     "<stop id=\"1\" node=\"321\" description=\"First\"/>\n"
@@ -106,7 +84,7 @@ TEST(XMLBusSystemTest, InvalidXMLTest) {
     auto BusRouteReader = std::make_shared<CXMLReader>(BusRouteSource);
 
     auto BusPathSource = std::make_shared<CStringDataSource>("<paths>\n"
-                                                                "<path source=\"321\" destination=\"311\"/>\n"
+                                                                "<path source=\"321\" destination=\"311\">\n"
                                                                     "<node id=\"321\"/>\n"
                                                                     "<node id=\"315\"/>\n"
                                                                     "<node id=\"311\"/>\n"
@@ -130,7 +108,7 @@ TEST(XMLBusSystemTest, InvalidXMLTest) {
     EXPECT_EQ(BusSystem.PathByStopIDs(1, 2), nullptr);
     EXPECT_EQ(BusSystem.PathByStopIDs(2, 3), nullptr);
 
-    // Test what happens if error is at the end (should read everything successfully prior to error)
+    // Test what happens if error is at the end
     BusRouteSource = std::make_shared<CStringDataSource>("<bussystem>\n"
                                                                 "<stops>\n"
                                                                     "<stop id=\"1\" node=\"321\" description=\"First\"/>\n"
@@ -138,34 +116,27 @@ TEST(XMLBusSystemTest, InvalidXMLTest) {
                                                             "</bussystem>");
     BusRouteReader = std::make_shared<CXMLReader>(BusRouteSource);
 
-    CXMLBusSystem BusSystem(BusRouteReader, BusPathReader);
+    CXMLBusSystem BusSystem2(BusRouteReader, BusPathReader);
 
-    EXPECT_EQ(BusSystem.StopCount(), 2);
-    EXPECT_EQ(BusSystem.RouteCount(), 0);
-    EXPECT_NE(BusSystem.StopByIndex(0), nullptr);
-    EXPECT_NE(BusSystem.StopByIndex(1), nullptr);
+    EXPECT_EQ(BusSystem2.StopCount(), 2);
+    EXPECT_EQ(BusSystem2.RouteCount(), 0);
+    EXPECT_NE(BusSystem2.StopByIndex(0), nullptr);
+    EXPECT_NE(BusSystem2.StopByIndex(1), nullptr);
     
-    auto StopObj = BusSystem.StopByID(1);
+    auto StopObj = BusSystem2.StopByID(1);
     ASSERT_NE(StopObj, nullptr);
     EXPECT_EQ(StopObj->ID(), 1);
     EXPECT_EQ(StopObj->NodeID(), 321);
     EXPECT_EQ(StopObj->Description(), "First");
 
-    StopObj = BusSystem.StopByID(2);
+    StopObj = BusSystem2.StopByID(2);
     ASSERT_NE(StopObj, nullptr);
     EXPECT_EQ(StopObj->ID(), 2);
     EXPECT_EQ(StopObj->NodeID(), 311);
     EXPECT_EQ(StopObj->Description(), "second");
 
-    auto PathObj = BusSystem.PathByStopIDs(1, 2);
-    ASSERT_NE(PathObj, nullptr);
-    EXPECT_EQ(PathObj->StartNodeID(), 321);
-    EXPECT_EQ(PathObj->EndNodeID(), 311);
-    EXPECT_EQ(PathObj->NodeCount(), 3);
-    EXPECT_EQ(PathObj->GetNodeID(0), 321);
-    EXPECT_EQ(PathObj->GetNodeID(1), 315);
-    EXPECT_EQ(PathObj->GetNodeID(2), 311);
-    EXPECT_EQ(PathObj->GetNodeID(3), CStreetMap::InvalidNodeID);
+    auto PathObj = BusSystem2.PathByStopIDs(1, 2);
+    EXPECT_EQ(PathObj, nullptr);
 }
 
 TEST(XMLBusSystemTest, ZeroTest) {
